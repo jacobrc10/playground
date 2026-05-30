103642fd7a687e626ace7a21794f66cf172b421d
Jacob Romano Carlsen <jromanocarlsen@gmail.com>
2026-05-30 13:57:17 -0700

    Added dedicated component for filters

---
+++ React/TanStack Query/recipe-explorer/src/components/FilterList.tsx
@@ -0,0 +1,50 @@
+function FilterList({
+  categories,
+  categoriesLoading,
+  categoriesError,
+  selectedCategories,
+  setSelectedCategories,
+}: {
+  categories: any;
+  categoriesLoading: boolean;
+  categoriesError: boolean;
+  selectedCategories: string[];
+  setSelectedCategories: (categories: string[]) => void;
+}) {
+  if (categoriesLoading) {
+    return <p>Loading categories...</p>;
+  }
+
+  if (categoriesError) {
+    return <p>Error fetching categories.</p>;
+  }
+
+  return (
+    <div className="categories">
+      <h2>Categories</h2>
+      <ul>
+        {categories.categories.map((category: any) => (
+          <li key={category.idCategory}>
+            <input
+              type="checkbox"
+              id={category.idCategory}
+              name={category.strCategory}
+              value={category.strCategory}
+              checked={selectedCategories.includes(category.strCategory)}
+              onChange={(e) => {
+                if (e.target.checked) {
+                  setSelectedCategories([...selectedCategories, category.strCategory]);
+                } else {
+                  setSelectedCategories(selectedCategories.filter((c) => c !== category.strCategory));
+                }
+              }}
+            />
+            <label htmlFor={category.idCategory}>{category.strCategory}</label>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default FilterList;
